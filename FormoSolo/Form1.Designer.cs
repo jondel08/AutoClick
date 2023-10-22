3a9e806cb09b0fa3716d1d61a3e7c88ab2cfe9da
Joam Delgado <36825976+jondel08@users.noreply.github.com>
2023-10-22 10:32:18 -0500

    Add files via upload

---
+++ FormoSolo/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace AutoClick {
+    partial class Form1 {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btn = new System.Windows.Forms.Button();
+            this.txt_time = new System.Windows.Forms.TextBox();
+            this.lbl_time = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPause = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btn
+            // 
+            this.btn.Location = new System.Drawing.Point(68, 128);
+            this.btn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btn.Name = "btn";
+            this.btn.Size = new System.Drawing.Size(107, 33);
+            this.btn.TabIndex = 0;
+            this.btn.Text = "Iniciar";
+            this.btn.UseVisualStyleBackColor = true;
+            this.btn.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // txt_time
+            // 
+            this.txt_time.Location = new System.Drawing.Point(118, 32);
+            this.txt_time.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txt_time.Name = "txt_time";
+            this.txt_time.Size = new System.Drawing.Size(59, 20);
+            this.txt_time.TabIndex = 1;
+            this.txt_time.Text = "20";
+            // 
+            // lbl_time
+            // 
+            this.lbl_time.AutoSize = true;
+            this.lbl_time.Location = new System.Drawing.Point(66, 36);
+            this.lbl_time.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbl_time.Name = "lbl_time";
+            this.lbl_time.Size = new System.Drawing.Size(48, 13);
+            this.lbl_time.TabIndex = 2;
+            this.lbl_time.Text = "Tiempo: ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(66, 83);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Pausa: ";
+            // 
+            // txtPause
+            // 
+            this.txtPause.Location = new System.Drawing.Point(118, 79);
+            this.txtPause.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtPause.Name = "txtPause";
+            this.txtPause.Size = new System.Drawing.Size(59, 20);
+            this.txtPause.TabIndex = 4;
+            this.txtPause.Text = "15";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(268, 208);
+            this.Controls.Add(this.txtPause);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbl_time);
+            this.Controls.Add(this.txt_time);
+            this.Controls.Add(this.btn);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = ".: Auto Click :.";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn;
+        private System.Windows.Forms.TextBox txt_time;
+        private System.Windows.Forms.Label lbl_time;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPause;
+    }
+}
+
---
+++ FormoSolo/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Timer = System.Windows.Forms.Timer;
+
+namespace AutoClick
+{
+    public partial class Form1 : Form
+    {
+        #region Variables
+        MessageBoxTemporal mBT = new MessageBoxTemporal();
+        private Timer timer;
+        private int tiempoSeg;
+        #endregion
+
+        #region Constructor
+        public Form1() {
+            InitializeComponent();
+        }
+        #endregion
+
+        #region Methods
+        private void Form1_Load(object sender, EventArgs e) {
+
+        }
+
+        //private void btn_Click(object sender, EventArgs e) {
+        //    tiempoSeg = Convert.ToInt32(txt_time.Text.Trim());
+        //    mBT.Show("Contando...", "Auto \"Click\"", tiempoSeg, true);
+        //    Thread.Sleep(3000);
+        //    btn.PerformClick();
+        //}
+
+        /// <summary>
+        /// Click del botón "Iniciar"
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btn_Click(object sender, EventArgs e) {
+            btn.Enabled = false;
+            //tiempoSeg = Convert.ToInt32(txt_time.Text.Trim());
+            tiempoSeg = ValidateTime();
+            mBT.Show("Contando...", "Auto \"Click\"", tiempoSeg, true);
+            timer = new Timer();
+            timer.Interval = ValidatePause() * 1000;
+            timer.Tick += Timer_Tick;
+            timer.Start();
+            btn.Enabled = true;
+        }
+
+        /// <summary>
+        /// Timer
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Timer_Tick(object sender, EventArgs e) {
+            timer.Stop();
+            btn.PerformClick();
+        }
+
+        /// <summary>
+        /// Valida el dato del campo "tiempo"
+        /// </summary>
+        /// <returns></returns>
+        private int ValidateTime() {
+            try {
+                if (Convert.ToInt32(txt_time.Text.Trim()) <= 0 || String.IsNullOrEmpty(txt_time.Text.Trim())) {
+                    MessageBox.Show("Tiempo: valor inválido. Se agrega valor default", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    txt_time.Text = "20";
+                    return 20;
+                }
+                else {
+                    return Convert.ToInt32(txt_time.Text.Trim());
+                }
+            }
+            catch (Exception ex) {
+                MessageBox.Show($"Tiempo: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txt_time.Text = "20";
+                return 20;
+            }
+
+        }
+
+        /// <summary>
+        /// Valida el dato del campo "pausa"
+        /// </summary>
+        /// <returns></returns>
+        private int ValidatePause() {
+            try {
+                if (Convert.ToInt32(txtPause.Text.Trim()) <= 0 || String.IsNullOrEmpty(txtPause.Text.Trim())) {
+                    MessageBox.Show("Pausa: valor inválido. Se agrega valor default", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    txtPause.Text = "15";
+                    return 15;
+                }
+                else {
+                    return Convert.ToInt32(txtPause.Text.Trim());
+                }
+            }
+            catch (Exception ex) {
+                MessageBox.Show($"Pausa: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtPause.Text = "15";
+                return 15;
+            }
+        }
+        #endregion
+
+    }//EndClass
+}//EndNamespace
